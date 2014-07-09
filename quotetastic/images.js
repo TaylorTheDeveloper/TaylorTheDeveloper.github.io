5c18f28b2b61ef3be1a83d81b751aadbe53d0737
TaylorTheDeveloper <TaylorTheDeveloper@gmail.com>
2014-07-09 16:33:20 -0400

    create data files

---
+++ quotetastic/images.js
@@ -0,0 +1,54 @@
+var images = [{
+    "boxx" : "25%",
+    "boxy" : "40%",
+    "image" : "oldspice_power.gif"
+},{
+    "boxx" : "25%",
+    "boxy" : "40%",
+    "image" : "waterfaucet.gif"
+},{
+    "boxx" : "3%",
+    "boxy" : "10%",
+    "image" : "umbrella.gif"
+},{
+    "boxx" : "48%",
+    "boxy" : "20%",
+    "image" : "shootingstar.gif"
+},{
+    "boxx" : "2%",
+    "boxy" : "40%",
+    "image" : "bicycle.gif"
+},{
+    "author" : "Ayn Rand",
+    "boxx" : "50%",
+    "boxy" : "8%",
+    "image" : "waterfall.gif"
+},{
+    "boxx" : "25%",
+    "boxy" : "40%",
+    "image" : "flower_focus.gif"
+},{
+    "boxx" : "2%",
+    "boxy" : "30%",
+    "image" : "cat_0001.gif"
+},{
+    "boxx" : "25%",
+    "boxy" : "40%",
+    "image" : "waterrefelctions.gif"
+},{
+    "boxx" : "50%",
+    "boxy" : "65%",
+    "image" : "fightclub_01.gif"
+},{
+    "boxx" : "25%",
+    "boxy" : "30%",
+    "image" : "hotspring.gif"
+},{
+    "boxx" : "45%",
+    "boxy" : "58%",
+    "image" : "intherain.gif"
+},{
+    "boxx" : "5%",
+    "boxy" : "15%",
+    "image" : "sparks.gif"
+}];
---
+++ quotetastic/quotes.js
@@ -0,0 +1,93 @@
+
+var quotes = [{
+    "quote" : "C'est en faisant n'importe quoi qu'on devient n'importe qui",
+    "author" : "Remi Gaillard",
+    "overrideImage" : "true",
+    "boxx" : "25%",
+    "boxy" : "40%",
+    "image" : "oldspice_power.gif"
+},{
+    "quote" : "If you tell the truth, you don't have to remember anything.",
+    "author" : "Mark Twain",
+    "overrideImage" : "false",
+    "boxx" : "25%",
+    "boxy" : "40%",
+    "image" : "waterfaucet.gif"
+},{
+    "quote" : "Those who dream by day are cognizant of many things that escape those who dream only at night.",
+    "author" : "Edgar Allen Poe",
+    "overrideImage" : "false",
+    "boxx" : "3%",
+    "boxy" : "10%",
+    "image" : "umbrella.gif"
+},{
+    "quote" : "The scientific man does not aim at an immediate result. He does not expect that his advanced ideas will be readily taken up. His work is like that of the planter - for the future. His duty is to lay the foundation for those who are to come, and point the way.",
+    "author" : "Nikola Tesla",
+    "overrideImage" : "true",
+    "boxx" : "48%",
+    "boxy" : "20%",
+    "image" : "shootingstar.gif"
+},{
+    "quote" : "I'm just a musical prostitute, my dear.",
+    "author" : "Freddie Mercury",
+    "overrideImage" : "false",
+    "boxx" : "2%",
+    "boxy" : "40%",
+    "image" : "bicycle.gif"
+},{
+    "quote" : "Achievement of your happiness is the only moral purpose of your life, and that happiness, not pain or mindless self-indulgence, is the proof of your moral integrity, since it is the proof and the result of your loyalty to the achievement of your values.",
+    "author" : "Ayn Rand",
+    "overrideImage" : "true",
+    "boxx" : "50%",
+    "boxy" : "8%",
+    "image" : "waterfall.gif"
+},{
+    "quote" : "To say 'I love you' one must first be able to say the 'I.'",
+    "author" : "Ayn Rand",
+    "overrideImage" : "false",
+    "boxx" : "25%",
+    "boxy" : "40%",
+    "image" : "flower_focus.gif"
+},{
+    "quote" : "Cloning represents a very clear, powerful, and immediate example in which we are in danger of turning procreation into manufacture.",
+    "author" : "Leon Kass",
+    "overrideImage" : "false",
+    "boxx" : "2%",
+    "boxy" : "30%",
+    "image" : "cat_0001.gif"
+},{
+    "quote" : "If any civilization is to survive, it is the morality of altruism that men have to reject.",
+    "author" : "Ayn Rand",
+    "overrideImage" : "false",
+    "boxx" : "25%",
+    "boxy" : "40%",
+    "image" : "waterrefelctions.gif"
+},{
+    "quote" : "It's only after you've lost everything that you're free to do anything.",
+    "author" : "Chuck Palahniuk",
+    "overrideImage" : "true",
+    "boxx" : "50%",
+    "boxy" : "65%",
+    "image" : "fightclub_01.gif"
+},{
+    "quote" : "He who controls others may be powerful, but he who has mastered himself is mightier still.",
+    "author" : "Lao Tzu",
+    "overrideImage" : "false",
+    "boxx" : "25%",
+    "boxy" : "30%",
+    "image" : "hotspring.gif"
+},{
+    "quote" : "Truth is, everybody is going to hurt you; you just gotta find the ones worth suffering for.",
+    "author" : "Bob Marley",
+    "overrideImage" : "false",
+    "boxx" : "45%",
+    "boxy" : "58%",
+    "image" : "intherain.gif"
+},{
+    "quote" : "If you realized how powerful your thoughts are, you would never think a negative thought.",
+    "author" : "P. Pilgrim",
+    "overrideImage" : "false",
+    "boxx" : "5%",
+    "boxy" : "15%",
+    "image" : "sparks.gif"
+}];
