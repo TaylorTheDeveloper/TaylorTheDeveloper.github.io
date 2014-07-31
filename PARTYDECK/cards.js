03ad68f72cba99663364ea01fde40ebab1ade5f8
TaylorTheDeveloper <TaylorTheDeveloper@gmail.com>
2014-07-31 14:34:50 -0400

    ui updates, card text updated

--- PARTYDECK/cards.js
+++ PARTYDECK/cards.js
@@ -17,17 +17,17 @@ angular.module('starter', ['ionic', 'ionic.contrib.ui.cards'])
 
 .controller('CardsCtrl', function($scope, $ionicSwipeCardDelegate) {
   var cardTypes = [
-    { title: 'Have a Drink!', image: 'img/solo.png', details:'' },
-    { title: 'Finish Your Drink!', image: 'img/solo.png', details:'' },
-    { title: 'Make a sex noise!', image: 'img/sexnoise.jpg', details:'' },
-    { title: 'Take a shot!', image: 'img/shot.png', details:'' },
-    { title: 'Rhyme Time!', image: 'img/solo.png', },//details:'You say a word and the person to your right has to say a word that rhymes. This goes around until someone can\'t think of a word. This person must drink. The same word can not be used twice.' },
-    { title: 'Mate!', image: 'img/twoshot.png', details:'You and whoever you want drink!(pick one)' },
-    { title: 'Alcoholics!', image: 'img/cheers.jpg', details:'Everybody Drinks' },
+    { title: 'Have a Drink!', image: 'img/solo.png', details:'Just a sip!' },
+    { title: 'Finish Your Drink!', image: 'img/solo.png', details:'CHUG!' },
+    { title: 'Make a sex noise!', image: 'img/sexnoise.jpg', details:'Last person Drinks!' },
+    { title: 'Take a shot!', image: 'img/shot.png', details:'lil\' Jon approves!' },
+    { title: 'Rhyme Time!', image: 'img/solo.png', details:''},//details:'You say a word and the person to your right has to say a word that rhymes. This goes around until someone can\'t think of a word. This person must drink. The same word can not be used twice.' },
+    { title: 'Mate!', image: 'img/twoshot.png', details:'You and one other person must Drink!' },
+    { title: 'Alcoholics!', image: 'img/cheers.jpg', details:'Everybody Drinks!' },
     { title: 'Take Two Shots!', image: 'img/twoshot.png', details:'' },
-    { title: 'Give a Wet Willy!', image: 'img/ear.png', details:'' },
-    { title: 'Get a Wet Willy!', image: 'img/ear.png', details:'' },
-    { title: 'Moose!', image: 'img/moose.png', },//details:'Whoever gets this card must immediately put their thumbs to their head with his/her fingers splayed, resembling moose antlers. The last person to do this must drink.' },
+    { title: 'Give a Wet Willy!', image: 'img/ear.png', details:'Muhahaha!' },
+    { title: 'Get a Wet Willy!', image: 'img/ear.png', details:'This is gonna suck!' },
+    { title: 'Moose!', image: 'img/moose.png', details:'Last person Drinks!'},//details:'Whoever gets this card must immediately put their thumbs to their head with his/her fingers splayed, resembling moose antlers. The last person to do this must drink.' },
     { title: 'Girls Drink!', image: 'img/female.png', details:'' },
     { title: 'Guys Drink!', image: 'img/male.png', details:'' }
   ];
